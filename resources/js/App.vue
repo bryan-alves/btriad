43891ffdf0bd3c2dc295376dc7e535b61568cebc
Bryan Alves <info.bryanalves@gmail.com>
2026-02-07 16:11:06 -0300

    feature: vue config

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Academia BTriad 🥋</h1>
+    <p>Vue + Laravel funcionando!</p>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+const message = 'Hello, Vue 3 with Laravel!';
+
+</script>
+
+<style>
+h1 {
+  color: #111;
+}
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <title>Academia</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
