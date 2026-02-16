f579c790d5973e1d616ad3a417692d4ac37e5102
Bryan Alves <info.bryanalves@gmail.com>
2026-02-15 22:34:30 -0300

    feat: started index landing page

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,21 @@
+<nav class="nav">
+  <div class="nav__container">
+    <div class="nav__logo">
+
+        <img class="mb-4" src="logo.png" alt="">
+    </div>
+
+    <button class="nav__toggle" aria-label="Abrir menu">
+      ☰
+    </button>
+
+    <ul class="nav__menu">
+      <li><a href="#sobre">Sobre</a></li>
+      <li><a href="#turmas">Turmas</a></li>
+      <li><a href="#horarios">Horários</a></li>
+      <li></li>
+    </ul>
+
+    <a href="#contato" class="nav__cta">Aula Experimental</a>
+  </div>
+</nav>
