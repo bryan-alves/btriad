6e00283bed9ec22347b51bd0bac29bbd700c356f
Bryan Alves <info.bryanalves@gmail.com>
2026-03-17 17:15:39 -0300

    feat: created FormInput component

---
+++ resources/js/components/form/FormInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  modelValue: String,
+  type: {
+    type: String,
+    default: 'text'
+  },
+  label: String,
+  placeholder: String,
+  error: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div>
+    <label class="font-medium">{{ label }}</label>
+    <input :type="type" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+      :placeholder="placeholder"
+      class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+    <span class="text-red-500 text-sm">{{ error }}</span>
+  </div>
+</template>
