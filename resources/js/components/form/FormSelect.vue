3ba01c1947ea4ddde6de7052053e80fa9264eb88
Bryan Alves <info.bryanalves@gmail.com>
2026-03-17 17:16:45 -0300

    feat: created FormSelect component

---
+++ resources/js/components/form/FormSelect.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  modelValue: String,
+  type: {
+    type: String,
+    default: 'text'
+  },
+  label: String,
+  placeholder: String,
+  options: {
+    type: Array,
+    default: () => []
+  },
+  error: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div>
+    <label class="font-medium">{{ label }}</label>
+    <select :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" name="" id=""
+      class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+      <option v-for="option in options" :key="option.value" :value="option.value">
+        {{ option.label }}
+      </option>
+    </select>
+    <span class="text-red-500 text-sm">{{ error }}</span>
+  </div>
+</template>
