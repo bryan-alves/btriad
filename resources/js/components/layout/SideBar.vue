32c8a46ed48ebe66cf476c7808c3ab3d4552a60d
Bryan Alves <info.bryanalves@gmail.com>
2026-03-18 16:50:33 -0300

    feat: added links

--- resources/js/components/layout/SideBar.vue
+++ resources/js/components/layout/SideBar.vue
@@ -3,9 +3,9 @@
     <img src="/public/logo-2.png" alt="" style="max-width: 150px;margin-bottom: 1rem">
     <h1 style="color: #FFF;font-size: 1.5rem">B-Triad Jiu-Jitsu</h1>
     <ul style="color: #FFF">
-      <li><RouterLink href="#">Dashboard</RouterLink></li>
+      <li><RouterLink to="#">Dashboard</RouterLink></li>
       <li><RouterLink to="/admin/students">Alunos</RouterLink></li>
-      <li><RouterLink href="#">Treinos</RouterLink></li>
+      <li><RouterLink to="#">Treinos</RouterLink></li>
     </ul>
   </div>
 </template>
