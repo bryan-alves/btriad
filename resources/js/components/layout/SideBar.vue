bf81d37f45267a7d1f75ff9d80c30bdc90d97086
Bryan Alves <info.bryanalves@gmail.com>
2026-03-17 22:14:06 -0300

    feat: created sidebar layout component

---
+++ resources/js/components/layout/SideBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="sidebar">
+    <img src="/public/logo-2.png" alt="" style="max-width: 150px;margin-bottom: 1rem">
+    <h1 style="color: #FFF;font-size: 1.5rem">B-Triad Jiu-Jitsu</h1>
+    <ul style="color: #FFF">
+      <li><RouterLink href="#">Dashboard</RouterLink></li>
+      <li><RouterLink to="/admin/students">Alunos</RouterLink></li>
+      <li><RouterLink href="#">Treinos</RouterLink></li>
+    </ul>
+  </div>
+</template>
