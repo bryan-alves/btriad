32542a8e55277767f47aac6f2d76dfdfd429b7d9
Bryan Alves <info.bryanalves@gmail.com>
2026-05-09 12:21:33 -0300

    feat: migrations

--- database/migrations/2026_03_15_204448_create_students_table.php
+++ database/migrations/2026_03_15_204448_create_students_table.php
@@ -14,21 +14,25 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('belt_id')
-                ->nullable()
-                ->constrained()
-                ->nullOnDelete();
-
             $table->foreignId('user_id')
                 ->nullable()
                 ->constrained()
                 ->nullOnDelete();
 
-            $table->enum('class_type', ['kids', 'adult']);
+            $table->foreignId('belt_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->date('degree')->nullable();
+
+            $table->string('name');
+
+            $table->string('cpf', 14)->unique()->nullable();
+
+            $table->enum('class_type', ['kids', 'adult'])->nullable();
 
             $table->string('photo')->nullable();
-            $table->string('name');
-            $table->string('cpf', 14)->unique()->nullable();
             $table->date('birth_date')->nullable();
             $table->string('sex')->nullable();
 
@@ -36,18 +40,17 @@ return new class extends Migration
 
             $table->json('emergency_contacts')->nullable();
 
-            $table->boolean('practices_other_sports')->default(false);
             $table->string('other_sports')->nullable();
 
             $table->text('health_issues')->nullable();
+
             $table->string('medical_certificate')->nullable();
 
             $table->string('registration_form_file')->nullable();
 
-            $table->boolean('image_authorization')->default(false);
-            $table->string('image_authorization_file')->nullable();
-
             $table->boolean('active')->default(true);
+
+            $table->date('first_class_at')->nullable();
 
             $table->timestamps();
         });
