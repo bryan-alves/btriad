be79a3703145e7543ad334b0155507815899d89d
Bryan Alves <info.bryanalves@gmail.com>
2026-05-09 11:36:23 -0300

    feat: changed belt seeder

--- database/seeders/BeltSeeder.php
+++ database/seeders/BeltSeeder.php
@@ -17,165 +17,111 @@ class BeltSeeder extends Seeder
                 'id' => 1,
                 'name' => 'Branca',
                 'slug' => 'white',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 2,
-                'name' => 'Branca',
-                'slug' => 'white',
-                'group' => 'adult',
-                'classes_per_stripe' => null,
+                'name' => 'Cinza e Branco',
+                'slug' => 'gray-white',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 3,
-                'name' => 'Cinza e Branco',
-                'slug' => 'gray-white',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Cinza',
+                'slug' => 'gray',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 4,
-                'name' => 'Cinza',
-                'slug' => 'gray',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Cinza e Preto',
+                'slug' => 'gray-black',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 5,
-                'name' => 'Cinza e Preto',
-                'slug' => 'gray-black',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Amarela e Branco',
+                'slug' => 'yellow-white',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 6,
-                'name' => 'Amarela e Branco',
-                'slug' => 'yellow-white',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Amarela',
+                'slug' => 'yellow',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 7,
-                'name' => 'Amarela',
-                'slug' => 'yellow',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Amarela e Preto',
+                'slug' => 'yellow-black',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 8,
-                'name' => 'Amarela e Preto',
-                'slug' => 'yellow-black',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Laranja',
+                'slug' => 'orange',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 9,
-                'name' => 'Laranja e Branco',
-                'slug' => 'orange-white',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Laranja e Preto',
+                'slug' => 'orange-black',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 10,
-                'name' => 'Laranja',
-                'slug' => 'orange',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Verde',
+                'slug' => 'green',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 11,
-                'name' => 'Laranja e Preto',
-                'slug' => 'orange-black',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Verde e Preto',
+                'slug' => 'green-black',
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 12,
-                'name' => 'Verde e Branco',
-                'slug' => 'green-white',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Azul',
+                'slug' => 'blue',
+
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 13,
-                'name' => 'Verde',
-                'slug' => 'green',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Roxa',
+                'slug' => 'purple',
+
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 14,
-                'name' => 'Verde e Preto',
-                'slug' => 'green-black',
-                'group' => 'kids',
-                'classes_per_stripe' => null,
+                'name' => 'Marrom',
+                'slug' => 'brown',
+
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'id' => 15,
-                'name' => 'Azul',
-                'slug' => 'blue',
-                'group' => 'adult',
-                'classes_per_stripe' => null,
-                'created_at' => $now,
-                'updated_at' => $now
-            ],
-            [
-                'id' => 16,
-                'name' => 'Roxa',
-                'slug' => 'purple',
-                'group' => 'adult',
-                'classes_per_stripe' => null,
-                'created_at' => $now,
-                'updated_at' => $now
-            ],
-            [
-                'id' => 17,
-                'name' => 'Marrom',
-                'slug' => 'brown',
-                'group' => 'adult',
-                'classes_per_stripe' => null,
-                'created_at' => $now,
-                'updated_at' => $now
-            ],
-            [
-                'id' => 18,
                 'name' => 'Preta',
                 'slug' => 'black',
-                'group' => 'adult',
-                'classes_per_stripe' => null,
+
                 'created_at' => $now,
                 'updated_at' => $now
             ]
-
         ]);
     }
 }
