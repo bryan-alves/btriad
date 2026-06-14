3c192e501b0e6bc65cb81bc006f016eb4f84f1d7
Bryan Alves <info.bryanalves@gmail.com>
2026-06-14 00:50:22 -0300

    feat

--- resources/views/home/credits.blade.php
+++ resources/views/home/credits.blade.php
@@ -1,6 +1,6 @@
 <div class="site-credit">
   <span>Desenvolvido por</span>
-  <a href="https://tatameiro.com.br/" target="_blank" rel="noopener noreferrer">
-    Tatameiro
+  <a href="https://tatameiro.com.br/" class="site-credit__brand" target="_blank" rel="noopener noreferrer">
+    TATA<span>MEIRO</span>
   </a>
 </div>
